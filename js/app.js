c57afad80576ea2753fee94a8b6dd4f9cee5b971
fujiwara ritsuki <>
2020-12-04 21:34:34 +0900

    no message

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-$(function (){
+$(function () {
 
+    // フロートヘッダーメニュー
     var targetHeight = $('.js-float-menu-target').height();
-    $(window).on('scroll',function(){
-        $('.js-float-menu').toggleClass('float-active',$(this).scrollTop() > targetHeight);
+    $(window).on('scroll', function() {
+      $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
     });
-
-    $('.js-toggle-sp-menu').on('click',function(){
-        $(this).toggleClass('active');
-        $('.js-toggle-sp-menu-target').toggleClass('active');
+  
+    // SPメニュー
+    $('.js-toggle-sp-menu').on('click', function () {
+      $(this).toggleClass('active');
+      $('.js-toggle-sp-menu-target').toggleClass('active');
     });
-});
+  });
